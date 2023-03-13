92a802e1de74727f6fd90552bc6ede383bf5fda3
Vlad Tsvigun <vl.tsv@outlook.com>
2023-03-13 21:03:13 +0200

    initial cleaning

--- main.py
+++ main.py
@@ -3,52 +3,22 @@ from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 import os
 
+# Load environment variables
 load_dotenv()
 
-client_id = os.getenv("SPOTIPY_CLIENT_ID")
-client_secret = os.getenv("SPOTIPY_CLIENT_SECRET")
-redirect_uri = os.getenv("SPOTIPY_REDIRECT_URI")
-
-# create a SpotifyOAuth instance
+# Initialize the Spotify API client
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
-        client_id=client_id,
-        client_secret=client_secret,
-        redirect_uri=redirect_uri,
-        # scope="user-library-read"
+        client_id=os.getenv("SPOTIPY_CLIENT_ID"),
+        client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"),
+        redirect_uri=os.getenv("SPOTIPY_REDIRECT_URI"),
+        # scope="playlist-read-private",
     )
 )
 
-sp.start_playback(uris=['spotify:track:1f3yAtsJtY87CTmM8RLnxf'])
+# Fetch the user's playlists
+results = sp.current_user_playlists()
 
-# def print_favorite_tracks():
-#     results = sp.current_user_saved_tracks(limit=50)
-#     for idx, item in enumerate(results['items']):
-#         track = item['track']
-#         print(f"{idx + 1}. {track['name']} by {track['artists'][0]['name']} from the album {track['album']['name']}")
-#
-#
-# def print_top_genres():
-#     results = sp.current_user_top_artists()
-#     genre_dict = {}
-#     for item in results['items']:
-#         genres = item['genres']
-#         for genre in genres:
-#             if genre in genre_dict:
-#                 genre_dict[genre] += 1
-#             else:
-#                 genre_dict[genre] = 1
-#     total_genres = sum(genre_dict.values())
-#     for genre, count in sorted(genre_dict.items(), key=lambda x: x[1], reverse=True):
-#         print(f"{genre}: {count / total_genres * 100:.2f}%")
-#
-#
-# def display_info():
-#     print("Favorite Tracks:\n")
-#     print_favorite_tracks()
-#     print("\nTop Genres:\n")
-#     print_top_genres()
-#
-#
-# if __name__ == '__main__':
-#     display_info()
+# Print the name of each playlist
+for item in results["items"]:
+    print(item["name"])
--- test.py
+++ test.py
@@ -1,24 +1,29 @@
+import os
+from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from dotenv import load_dotenv
-import os
 
-# Load environment variables
+# Load environment variables from .env file
 load_dotenv()
+client_id = os.getenv("SPOTIPY_CLIENT_ID")
+client_secret = os.getenv("SPOTIPY_CLIENT_SECRET")
+redirect_uri = os.getenv("SPOTIPY_REDIRECT_URI")
 
-# Initialize the Spotify API client
+# Create spotipy authentication object
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
-        client_id=os.getenv("SPOTIPY_CLIENT_ID"),
-        client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"),
-        redirect_uri=os.getenv("SPOTIPY_REDIRECT_URI"),
-        # scope="playlist-read-private",
+        client_id=client_id,
+        client_secret=client_secret,
+        redirect_uri=redirect_uri,
+        scope="user-top-read"
     )
 )
 
-# Fetch the user's playlists
-results = sp.current_user_playlists()
+# Get user's top 10 artists
+top_artists = sp.current_user_top_artists(limit=10, time_range="medium_term")
 
-# Print the name of each playlist
-for item in results["items"]:
-    print(item["name"])
+# Print the name of the artist and the number of followers
+for artist in top_artists["items"]:
+    # Fetch detailed artist information
+    artist_info = sp.artist(artist["id"])
+    print(artist_info["name"], "-", artist_info["followers"]["total"], "followers")
