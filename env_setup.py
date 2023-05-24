6eaa8117dc5593b423465c1f2b66ea33bee53b76
vldtsw <vl.tsv@outlook.com>
2023-05-24 20:52:34 +0300

    env_setup script updated

---
+++ env_setup.py
@@ -0,0 +1,27 @@
+env_variables = {
+    "SPOTIPY_CLIENT_ID": None,
+    "SPOTIPY_CLIENT_SECRET": None,
+    "SPOTIPY_REDIRECT_URI": None,
+}
+
+
+# Function to get values for the environment variables from user input
+def get_env_variable_values():
+    for key in env_variables:
+        value = input(f"Enter the value for '{key}': ")
+        env_variables[key] = value
+
+
+# Function to generate the .env file
+def generate_env_file():
+    with open(".env", "w") as file:
+        for key, value in env_variables.items():
+            file.write(f"{key}={value}\n")
+
+
+# Main execution
+if __name__ == "__main__":
+    print("Enter the values for the environment variables:")
+    get_env_variable_values()
+    generate_env_file()
+    print(".env file has been generated successfully.")
